39176a30ddd8be771b8428f32f045b57017fa83f
wujianguo <jianguo.wu@zhipuai.cn>
2024-01-29 13:58:43 +0800

    sse结果输出移到最外层

--- src/main/java/com/zhipu/oapi/core/httpclient/OkHttpTransport.java
+++ src/main/java/com/zhipu/oapi/core/httpclient/OkHttpTransport.java
@@ -30,19 +30,7 @@ public class OkHttpTransport extends BaseHttpTransport {
     private OkHttpClient okHttpClient;
 
 
-    private static final ObjectMapper mapper = defaultObjectMapper();
 
-
-    public static ObjectMapper defaultObjectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-        mapper.addMixIn(ChatFunction.class, ChatFunctionMixIn.class);
-        mapper.addMixIn(ChatCompletionRequest.class, ChatCompletionRequestMixIn.class);
-        mapper.addMixIn(ChatFunctionCall.class, ChatFunctionCallMixIn.class);
-        return mapper;
-    }
 
 
     public OkHttpTransport(OkHttpClient okHttpClient) {
@@ -100,47 +88,17 @@ public class OkHttpTransport extends BaseHttpTransport {
         ChatApiService service = new ChatApiService(request.getToken());
         RawResponse resp = new RawResponse();
         Flowable<ModelData> flowable;
-        Map<String, Object> data = new HashMap<>();
-        data.put("request_id", request.getBody().get("request_id"));
         try {
             flowable  = service.streamChatCompletion(request.getBody());
         } catch (Exception e) {
             System.out.println("streamChatCompletion error:" + e.getMessage());
             resp.setStatusCode(500);
             resp.setSuccess(false);
-            data.put("task_status", TaskStatus.FAIL);
-            resp.setBody(new Gson().toJson(data));
             return resp;
         }
-        AtomicBoolean isFirst = new AtomicBoolean(true);
-        ChatMessageAccumulator chatMessageAccumulator = service.mapStreamToAccumulator(flowable)
-                .doOnNext(accumulator -> {
-                    {
-                        if (isFirst.getAndSet(false)) {
-                            System.out.print("Response: ");
-                        }
-                        if (accumulator.getDelta() != null && accumulator.getDelta().getTool_calls() != null) {
-                            String jsonString = mapper.writeValueAsString(accumulator.getDelta().getTool_calls());
-                            System.out.println("tool_calls: "+jsonString);
-                        }
-                        if (accumulator.getDelta() != null && accumulator.getDelta().getContent() != null) {
-                            System.out.print(accumulator.getDelta().getContent());
-                        }
-                    }
-                })
-                .doOnComplete(System.out::println)
-                .lastElement()
-                .blockingGet();
         resp.setSuccess(true);
         resp.setStatusCode(200);
-        Choice choice = new Choice(chatMessageAccumulator.getChoice().getFinishReason(), 0L, chatMessageAccumulator.getDelta());
-        List<Choice> choices = new ArrayList<>();
-        choices.add(choice);
-        data.put("choices", choices);
-        data.put("usage", chatMessageAccumulator.getUsage());
-        data.put("id", chatMessageAccumulator.getId());
-        data.put("created", chatMessageAccumulator.getCreated());
-        resp.setBody(new Gson().toJson(data));
+        resp.setFlowable(flowable);
         return resp;
     }
 
--- src/main/java/com/zhipu/oapi/demo/V4OkHttpClientTest.java
+++ src/main/java/com/zhipu/oapi/demo/V4OkHttpClientTest.java
@@ -1,28 +1,47 @@
 package com.zhipu.oapi.demo;
 
 import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.zhipu.oapi.ClientV4;
 import com.zhipu.oapi.Constants;
-import com.zhipu.oapi.service.v4.fine_turning.*;
-import com.zhipu.oapi.service.v4.model.*;
 import com.zhipu.oapi.service.v4.embedding.EmbeddingApiResponse;
 import com.zhipu.oapi.service.v4.embedding.EmbeddingRequest;
 import com.zhipu.oapi.service.v4.file.FileApiResponse;
 import com.zhipu.oapi.service.v4.file.QueryFileApiResponse;
 import com.zhipu.oapi.service.v4.file.QueryFilesRequest;
+import com.zhipu.oapi.service.v4.fine_turning.*;
 import com.zhipu.oapi.service.v4.image.CreateImageRequest;
 import com.zhipu.oapi.service.v4.image.ImageApiResponse;
+import com.zhipu.oapi.service.v4.model.*;
+import io.reactivex.Flowable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 public class V4OkHttpClientTest {
 
     private static final ClientV4 client = new ClientV4.Builder(Constants.onlineKeyV3, Constants.onlineSecretV3).build();
+
+    private static final ObjectMapper mapper = defaultObjectMapper();
+
+
+    public static ObjectMapper defaultObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        mapper.addMixIn(ChatFunction.class, ChatFunctionMixIn.class);
+        mapper.addMixIn(ChatCompletionRequest.class, ChatCompletionRequestMixIn.class);
+        mapper.addMixIn(ChatFunctionCall.class, ChatFunctionCallMixIn.class);
+        return mapper;
+    }
 
     // 请自定义自己的业务id
     private static final String requestIdTemplate = "mycompany-%d";
@@ -181,8 +200,8 @@ public class V4OkHttpClientTest {
      */
      private static void testSseInvoke() {
          List<ChatMessage> messages = new ArrayList<>();
-//         ChatMessage chatMessage = new ChatMessage(ChatMessageRole.USER.value(), "您好，北京天气怎么样");
-         ChatMessage chatMessage = new ChatMessage(ChatMessageRole.USER.value(), "你能帮我查询2024年1月1日从北京南站到上海的火车票吗？");
+         ChatMessage chatMessage = new ChatMessage(ChatMessageRole.USER.value(), "您好，北京天气怎么样");
+//         ChatMessage chatMessage = new ChatMessage(ChatMessageRole.USER.value(), "你能帮我查询2024年1月1日从北京南站到上海的火车票吗？");
          messages.add(chatMessage);
          String requestId = String.format(requestIdTemplate, System.currentTimeMillis());
          // 函数调用参数构建部分
@@ -227,10 +246,47 @@ public class V4OkHttpClientTest {
                  .toolChoice("auto")
                  .build();
          ModelApiResponse sseModelApiResp = client.invokeModelApi(chatCompletionRequest);
+         if(sseModelApiResp.isSuccess()){
+            AtomicBoolean isFirst = new AtomicBoolean(true);
+            ChatMessageAccumulator chatMessageAccumulator = mapStreamToAccumulator(sseModelApiResp.getFlowable())
+                    .doOnNext(accumulator -> {
+                        {
+                            if (isFirst.getAndSet(false)) {
+                                System.out.print("Response: ");
+                            }
+                            if (accumulator.getDelta() != null && accumulator.getDelta().getTool_calls() != null) {
+                                String jsonString = mapper.writeValueAsString(accumulator.getDelta().getTool_calls());
+                                System.out.println("tool_calls: "+jsonString);
+                            }
+                            if (accumulator.getDelta() != null && accumulator.getDelta().getContent() != null) {
+                                System.out.print(accumulator.getDelta().getContent());
+                            }
+                        }
+                    })
+                    .doOnComplete(System.out::println)
+                    .lastElement()
+                    .blockingGet();
+
+            Choice choice = new Choice(chatMessageAccumulator.getChoice().getFinishReason(), 0L, chatMessageAccumulator.getDelta());
+            List<Choice> choices = new ArrayList<>();
+            choices.add(choice);
+             ModelData data = new ModelData();
+             data.setChoices(choices);
+             data.setUsage(chatMessageAccumulator.getUsage());
+             data.setId(chatMessageAccumulator.getId());
+             data.setCreated(chatMessageAccumulator.getCreated());
+             data.setRequestId(chatCompletionRequest.getRequestId());
+             sseModelApiResp.setFlowable(null);
+             sseModelApiResp.setData(data);
+        }
          System.out.println("model output:"+ JSON.toJSONString(sseModelApiResp));
      }
 
-
+    public static Flowable<ChatMessageAccumulator> mapStreamToAccumulator(Flowable<ModelData> flowable) {
+        return flowable.map(chunk -> {
+            return new ChatMessageAccumulator(chunk.getChoices().get(0).getDelta(), null,chunk.getChoices().get(0),chunk.getUsage(),chunk.getCreated(),chunk.getId());
+        });
+    }
     /**
      * 同步调用
      */
