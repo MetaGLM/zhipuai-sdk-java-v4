733d3fe57cd99f01185698f927c5ccf3103bea71
glide-the <2533736852@qq.com>
2024-05-08 14:52:24 +0800

    修改一些问题

--- src/test/java/com/zhipu/oapi/V4Test.java
+++ src/test/java/com/zhipu/oapi/V4Test.java
@@ -33,7 +33,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class V4Test {
 
     private final static Logger logger = LoggerFactory.getLogger(V4Test.class);
-    private static final String API_SECRET_KEY = ".";
+    private static final String API_SECRET_KEY = "a9733a59370d34ef51d261bd461251a5.YD9PU7sAqUwgD8Sk";
     private static final boolean devMode = false;
 
 
@@ -309,15 +309,12 @@ public class V4Test {
 
         HashMap<String, Object> extraJson = new HashMap<>();
         extraJson.put("temperature", 0.5);
-        String metaString = "{\n" +
-                "                \"user_info\": \"我是陆星辰，是一个男性，是一位知名导演，也是苏梦远的合作导演。我擅长拍摄音乐题材的电影。苏梦远对我的态度是尊敬的，并视我为良师益友。\",\n" +
-                "                \"bot_info\": \"苏梦远，本名苏远心，是一位当红的国内女歌手及演员。在参加选秀节目后，凭借独特的嗓音及出众的舞台魅力迅速成名，进入娱乐圈。她外表美丽动人，但真正的魅力在于她的才华和勤奋。苏梦远是音乐学院毕业的优秀生，善于创作，拥有多首热门原创歌曲。除了音乐方面的成就，她还热衷于慈善事业，积极参加公益活动，用实际行动传递正能量。在工作中，她对待工作非常敬业，拍戏时总是全身心投入角色，赢得了业内人士的赞誉和粉丝的喜爱。虽然在娱乐圈，但她始终保持低调、谦逊的态度，深得同行尊重。在表达时，苏梦远喜欢使用“我们”和“一起”，强调团队精神。\",\n" +
-                "                \"bot_name\": \"苏梦远\",\n" +
-                "                \"user_name\": \"陆星辰\"\n" +
-                "            }";
 
-        ObjectNode objectNode = (ObjectNode) mapper.readTree(metaString);
-        ChatMeta meta = new ChatMeta(objectNode);
+        ChatMeta meta = new ChatMeta();
+        meta.setUser_info("我是陆星辰，是一个男性，是一位知名导演，也是苏梦远的合作导演。我擅长拍摄音乐题材的电影。苏梦远对我的态度是尊敬的，并视我为良师益友。");
+        meta.setBot_info("苏梦远，本名苏远心，是一位当红的国内女歌手及演员。在参加选秀节目后，凭借独特的嗓音及出众的舞台魅力迅速成名，进入娱乐圈。她外表美丽动人，但真正的魅力在于她的才华和勤奋。苏梦远是音乐学院毕业的优秀生，善于创作，拥有多首热门原创歌曲。除了音乐方面的成就，她还热衷于慈善事业，积极参加公益活动，用实际行动传递正能量。在工作中，她对待工作非常敬业，拍戏时总是全身心投入角色，赢得了业内人士的赞誉和粉丝的喜爱。虽然在娱乐圈，但她始终保持低调、谦逊的态度，深得同行尊重。在表达时，苏梦远喜欢使用“我们”和“一起”，强调团队精神。");
+        meta.setBot_name("苏梦远");
+        meta.setUser_name("陆星辰");
 
         ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
                 .model(Constants.ModelCharGLM3)
