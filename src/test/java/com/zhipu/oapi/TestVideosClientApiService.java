518d75780ee8196cddb8956545cf33cd462bbf16
glide-the <2533736852@qq.com>
2024-07-29 12:08:29 +0800

    更新模型名称

--- src/test/java/com/zhipu/oapi/TestVideosClientApiService.java
+++ src/test/java/com/zhipu/oapi/TestVideosClientApiService.java
@@ -49,7 +49,7 @@ public class TestVideosClientApiService {
 
         VideoCreateParams build = VideoCreateParams.builder()
                 .prompt("一个开船的人")
-                .model("cogvideo")
+                .model("cogvideox")
                 .build();
         VideosResponse apply = new VideosClientApiService(client.getConfig().getHttpClient(), client.getConfig().getBaseUrl())
                 .videoGenerations(build)
@@ -67,7 +67,7 @@ public class TestVideosClientApiService {
                 .prompt(
                           "这个场景描绘了一个充满魔法气息的场景。在一个被绿色植物和橙色小花环绕的石桌上，一本古老的书籍摊开着，书页似乎刚刚被翻动过。书籍的旁边放置着一个透明的魔法球，魔法球中充满了闪烁的光点和流动的光影，仿佛有一种神秘的能量在其中流转。魔法球下方的底座散发出微弱的光芒，周围的空气中漂浮着一些细小的光粒，增强了场景的神秘感和魔幻氛围。背景中隐约可见一些模糊的建筑结构，进一步烘托出这个场景的奇幻与神秘。")
                 .imageUrl(imageUrl)
-                .model("cogvideo")
+                .model("cogvideox")
                 .build();
         VideosResponse apply = new VideosClientApiService(client.getConfig().getHttpClient(), client.getConfig().getBaseUrl())
                 .videoGenerations(build)
